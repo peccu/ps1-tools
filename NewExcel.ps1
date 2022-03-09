1f8c03c55f9e83c4af3ba6af2062fe636b2772a9
peccu <peccul@gmail.com>
2022-03-09 10:13:54 +0000

    initial commit

---
+++ NewExcel.ps1
@@ -0,0 +1,5 @@
+﻿function new-excel {
+  (New-Object -ComObject Excel.Application).Visible = $true
+  Read-Host -Prompt "Press Enter to exit (also kills Excel)"
+}
+new-excel
---
+++ SwitchJabra.ps1
@@ -0,0 +1,14 @@
+﻿# based on https://community.spiceworks.com/topic/2292318-select-audio-device-with-powershell
+If (! (Get-Module -Name "AudioDeviceCmdlets" -ListAvailable)) {
+  # but needs Admin
+  Install-Module -Name AudioDeviceCmdlets -Force -Verbose  
+  get-module -Name "AudioDeviceCmdlets" -ListAvailable | Sort-Object Version | select -last 1 | Import-Module -Verbose
+}
+
+if ( Get-AudioDevice -Recording | where name -like "* SPEAK *"){
+  echo "JabraSPEAK -> JabraLink"
+  Get-AudioDevice -List | where name -like "* Link *" | Set-AudioDevice -Verbose
+}else{
+  echo "JabraLink -> JabraSPEAK"
+  Get-AudioDevice -List | where name -like "* SPEAK *" | Set-AudioDevice -Verbose
+}
